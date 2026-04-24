1403b61fb2f96cbf4a1a15a259eadda7bd76170e
Mihail <misterslender552@gmail.com>
2026-04-24 18:07:46 +0500

    Add illegalArgument Exception handler

--- src/main/java/com/tpm/task_service/controller/GlobalExceptionHandler.java
+++ src/main/java/com/tpm/task_service/controller/GlobalExceptionHandler.java
@@ -12,6 +12,14 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<ResponseDto<Void>> handleIllegalArgument(IllegalArgumentException ex) {
+        return ResponseEntity
+                .badRequest()
+                .body(ResponseDto.error("IllegalArgumentException",ex.getMessage()));
+    }
+
     @ExceptionHandler(UserNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ResponseEntity<ResponseDto<Void>> handleUserNotFound(UserNotFoundException ex) {
